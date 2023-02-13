f47507971d95f69ccce3efd0a3f829d8defb2592
Fabricio Moreno <fabricio.morenonaranjo@hotmail.com>
2023-02-13 18:58:14 -0500

    About section added

---
+++ pages/about-us.js
@@ -0,0 +1,12 @@
+
+
+const Page = ()=>{
+    return(
+        <div>
+            <h1>About-us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero possimus alias, earum aliquam nesciunt magnam debitis fuga modi eligendi consectetur sunt. Adipisci autem minus repudiandae quod voluptatum atque itaque harum!</p>
+        </div>
+    )
+}
+
+export default Page
