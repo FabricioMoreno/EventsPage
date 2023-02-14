4c6fe0f933b95b11adad1f1580f3db553cd9af8c
Fabricio Moreno <fabricio.morenonaranjo@hotmail.com>
2023-02-13 19:01:26 -0500

    Globals componentes added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import Footer from '@/src/components/footer/footer'
+import Header from '@/src/components/header/header'
+import MainLayout from '@/src/components/layout/Layout'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  
+  </>
+  )
 }
